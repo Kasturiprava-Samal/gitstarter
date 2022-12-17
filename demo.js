5d5d833767a8bc76ee5dd9a1da172cdfd3905504
Kasturiprava-Samal <kasturipravasamal.official@gmail.com>
2022-12-18 00:42:22 +0530

    using javascript.

--- demo.js
+++ demo.js
@@ -1,1 +1,12 @@
-console.log("Kasturiprava Samal")
+//console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+console.log(headerTitle)
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000' ;
+for (var i = 0 ;i<items.length;i++){
+    items[i].style.backgroundcolor = '#00ff00';
+}
+document.getElementById('Main').style.color = "green";
+var Main = document.getElementById('Main').style.color = "green";
+//Main.fontcolor('#00ff00');
+console.log(Main)
