5103e55a11080ef89ea2a33dadadcc6a00262d72
Kasturiprava-Samal <kasturipravasamal.official@gmail.com>
2023-01-04 17:17:22 +0530

    getelementbyid

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("I love git")
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[3]);
+//items[3].textContent='Hello 2';
+//items[3].style.fontWeight='bold';
+items[2].style.backgroundColor = 'Green';
+
+for(var i=0; i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
